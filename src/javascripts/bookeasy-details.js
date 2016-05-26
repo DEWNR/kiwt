7239395bcb2b5d218e733d8eda124d00765a983e
Ben Kannenberg <bkannenberg@gmail.com>
2016-05-27 09:24:03 +0930

    style bookeasy a little more

--- src/javascripts/bookeasy-details.js
+++ src/javascripts/bookeasy-details.js
@@ -1,18 +1,10 @@
 var bAllowBookings = true;
 var bookeasyData = {};
-var bookeasyType = 'accom'; // defaults to accomodation
+var bookeasyType = 'tours'; // defaults to accomodation
 var bookingDate = new Date(); // get today's date
-var operatorIDs = productID = 80122; // set operatorIDs to display
-var operatorIDString = operatorIDs.join(); // create a string for the JSONP request
-var operatorPageType = 'tours'; // type of operator
-
-if (operatorPageType == 'tours') {
-    bookeasyType = 'tours';
-}
+var productID = 80122; // set operatorIDs to display
 
 $(function() {
-
-    getOperatorData();
 
     // load details gadget
     BE.gadget.details('#bookeasy__details-gadget', {
@@ -24,42 +16,13 @@ $(function() {
         showQuantity: false,
         thumbsInGrid: true,
         type: bookeasyType,
-        noAdults: 10,
-        noChildren: 10,
-        noStudents: 10,
-        noConcessions: 10,
+        noAdults: 12,
+        noChildren: 12,
+        noStudents: 12,
+        noConcessions: 12,
         noInfants: 0,
         noObservers: 0,
         vcID: 188
     });
 
 });
-
-function getOperatorData() {
-
-    $.ajax({
-        url: '//sjp.impartmedia.com/be/getOperatorsDetailsShort?q=188&operators=' + operatorIDString,
-        cache: true,
-        dataType: 'jsonp',
-        jsonpCallback: 'jQueryOperatorsInformation'
-    })
-    .done(function(data) {
-
-        if (data.length) {
-
-            // insert t&c information
-            var cancellationPolicy = data[0].CancellationPolicy;
-
-            $('#bookeasy__conditions').html('<h3>Terms and conditions</h3>' + text2HTML(cancellationPolicy));
-        }
-
-    })
-
-}
-
-function text2HTML(input) {
-    input = input.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\"/g, '&quot;').replace(/\//g, '/');
-    input = input.replace(/\r/g, '').replace(/\n( )*?(\n)*?( )*?$/, '').replace(/\n( )*?\n/g, '</p><p>').replace(/\n/g, '<br />');
-    input = '<p>'+input+'</p>';
-    return input;
-}
