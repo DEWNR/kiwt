07f8b1ddc9fe9778e93f8e788813343988e547e9
Ben Kannenberg <bkannenberg@gmail.com>
2016-05-31 14:08:28 +0930

    Lots of style tweaks

--- src/javascripts/bookeasy-details.js
+++ src/javascripts/bookeasy-details.js
@@ -3,16 +3,24 @@ var bookeasyData = {};
 var bookeasyType = 'tours'; // defaults to accomodation
 var bookingDate = new Date(); // get today's date
 var productID = 80122; // set operatorIDs to display
+var availbleDate = new Date(2016, 10 - 1, 1);
+var todaysDate = new Date();
+
+bookingDate.setDate(bookingDate.getDate() + 1);
+
+if (availbleDate > todaysDate) {
+    bookingDate = availbleDate;
+}
 
 $(function() {
 
     // load details gadget
     BE.gadget.details('#bookeasy__details-gadget', {
         defaultDate: bookingDate,
-        descriptionHover: true,
+        descriptionHover: false,
         period: 1,
         productID: productID,
-        showHoverInline: true,
+        showHoverInline: false,
         showQuantity: false,
         thumbsInGrid: true,
         type: bookeasyType,
@@ -22,6 +30,8 @@ $(function() {
         noConcessions: 12,
         noInfants: 0,
         noObservers: 0,
+        period: 1,
+        adults: 1,
         vcID: 188
     });
 
