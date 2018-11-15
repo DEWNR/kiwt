b169bfb05849b1526cffad96b8646693a2569570
Liam Rella <Liam.Rella@sa.gov.au>
2018-11-15 13:29:15 +1030

    fix json gulp task

--- gulpfile.js/tasks/html.js
+++ gulpfile.js/tasks/html.js
@@ -1,52 +1,61 @@
-var config       = require('../config')
-if(!config.tasks.html) return
+var config = require('../config');
+if (!config.tasks.html) return;
 
-var browserSync  = require('browser-sync')
-var data         = require('gulp-data')
-var gulp         = require('gulp')
-var gulpif       = require('gulp-if')
-var handleErrors = require('../lib/handleErrors')
-var htmlmin      = require('gulp-htmlmin')
-var path         = require('path')
-var render       = require('gulp-nunjucks-render')
-var fs           = require('fs')
-var jsoncombine  = require("gulp-jsoncombine")
-var gulpSequence = require('gulp-sequence')
+var browserSync = require('browser-sync');
+var data = require('gulp-data');
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var handleErrors = require('../lib/handleErrors');
+var htmlmin = require('gulp-htmlmin');
+var path = require('path');
+var render = require('gulp-nunjucks-render');
+var fs = require('fs');
+var jsoncombine = require('gulp-jsoncombine');
+var gulpSequence = require('gulp-sequence');
 
-var exclude = path.normalize('!**/{' + config.tasks.html.excludeFolders.join(',') + '}/**')
+var exclude = path.normalize(
+    '!**/{' + config.tasks.html.excludeFolders.join(',') + '}/**'
+);
 
 var paths = {
-  src: [path.join(config.root.src, config.tasks.html.src, '/**/*.html'), exclude],
-  dest: path.join(config.root.dest, config.tasks.html.dest),
-}
+    src: [
+        path.join(config.root.src, config.tasks.html.src, '/**/*.html'),
+        exclude
+    ],
+    dest: path.join(config.root.dest, config.tasks.html.dest)
+};
 
 var getData = function(file) {
-  var dataPath = path.resolve(config.root.src, config.tasks.html.src, config.tasks.html.dataFile)
-  var data;
+    var dataPath = path.resolve(
+        config.root.src,
+        config.tasks.html.src,
+        config.tasks.html.dataFile
+    );
 
-  try {
-    data = fs.readFileSync(dataPath, 'utf8');
-  } catch (e) {
-    data = '';
-  }
-
-  return data
-}
-
-
+    return JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+};
 
 var htmlTask = function(cb) {
-    render.nunjucks.configure([path.join(config.root.src, config.tasks.html.src)], {watch: false })
+    render.nunjucks.configure(
+        [path.join(config.root.src, config.tasks.html.src)],
+        { watch: false }
+    );
 
-    return gulp.src(paths.src)
-      .pipe(data(getData))
-      .on('error', handleErrors)
-      .pipe(render())
-      .on('error', handleErrors)
-      .pipe(gulpif(process.env.NODE_ENV == 'production', htmlmin(config.tasks.html.htmlmin)))
-      .pipe(gulp.dest(paths.dest))
-      .pipe(browserSync.stream())
-}
+    return gulp
+        .src(paths.src)
+        .pipe(data(getData))
+        .on('error', handleErrors)
+        .pipe(render())
+        .on('error', handleErrors)
+        .pipe(
+            gulpif(
+                process.env.NODE_ENV == 'production',
+                htmlmin(config.tasks.html.htmlmin)
+            )
+        )
+        .pipe(gulp.dest(paths.dest))
+        .pipe(browserSync.stream());
+};
 
-gulp.task('html', ['json'], htmlTask)
-module.exports = htmlTask
+gulp.task('html', ['json'], htmlTask);
+module.exports = htmlTask;
--- gulpfile.js/tasks/json.js
+++ gulpfile.js/tasks/json.js
@@ -1,22 +1,30 @@
-var config       = require('../config')
-var gulp         = require('gulp')
-var jsoncombine  = require("gulp-jsoncombine")
-var path         = require('path')
+var config = require('../config');
+var gulp = require('gulp');
+var jsoncombine = require('gulp-jsoncombine');
+var path = require('path');
+var _ = require('lodash');
 
 var jsonTask = function(cb) {
+    console.log('Starting jsonTask');
+    var dataFiles = [
+        path.resolve(config.root.src, config.tasks.html.src, 'data/*.json'),
+        '!**/global.json'
+    ];
 
- var dataFiles = path.resolve(config.root.src, config.tasks.html.src, 'data/*.json')
+    return gulp
+        .src(dataFiles)
+        .pipe(
+            jsoncombine('global.json', function(data) {
+                var dataCC = _.mapKeys(data, function(v, k) {
+                    return _.camelCase(k);
+                });
+                var sData = JSON.stringify(dataCC);
 
- return gulp.src(dataFiles)
-   .pipe(jsoncombine("global.json",function(data){
-       var sData = JSON.stringify(data)
-       var sDataCC = sData.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase(); })
+                return new Buffer(sData);
+            })
+        )
+        .pipe(gulp.dest('./src/html/data'));
+};
 
-       return new Buffer(sDataCC)
-   }))
-   .pipe(gulp.dest("./src/html/data"));
-
-}
-
-gulp.task('json', jsonTask)
-module.exports = jsonTask
+gulp.task('json', jsonTask);
+module.exports = jsonTask;
